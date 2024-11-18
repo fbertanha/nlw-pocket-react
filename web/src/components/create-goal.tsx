3a60bd1ec811aa2b945f44b1e38c2397548fe334
Felipe Bertanha <fbertanha.dev@gmail.com>
2024-11-18 23:19:09 +0000

    Integrated  backend calls to create goals and completions.

--- web/src/components/create-goal.tsx
+++ web/src/components/create-goal.tsx
@@ -13,8 +13,55 @@ import {
   RadioGroupIndicator,
   RadioGroupItem,
 } from './ui/radio-group'
+import { z } from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createGoal } from '../http/create-goal'
+import { useQueryClient } from '@tanstack/react-query'
+
+const createGoalForm = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja realizar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type WeeklyOption = {
+  title: string
+  value: string
+  icon: string
+}
+
+const weeklyOptions: WeeklyOption[] = [
+  { title: '1x na Semana', value: '1', icon: '🥱' },
+  { title: '2x na Semana', value: '2', icon: '🙂' },
+  { title: '3x na Semana', value: '3', icon: '😎' },
+  { title: '4x na Semana', value: '4', icon: '😜' },
+  { title: '5x na Semana', value: '5', icon: '🤨' },
+  { title: '6x na Semana', value: '6', icon: '🤯' },
+  { title: '7x na Semana', value: '7', icon: '🔥' },
+]
+
+type CreateGoalForm = z.infer<typeof createGoalForm>
 
 export function CreateGoal() {
+  const queryClient = useQueryClient()
+
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<CreateGoalForm>({
+      resolver: zodResolver(createGoalForm),
+    })
+
+  async function handleCreateGoal(data: CreateGoalForm) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    })
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+
+    reset()
+  }
+
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
@@ -30,7 +77,10 @@ export function CreateGoal() {
             praticando toda semana.
           </DialogDescription>
         </div>
-        <form action="" className="flex-1 flex flex-col justify-between">
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex-1 flex flex-col justify-between"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual a atividade?</Label>
@@ -38,34 +88,47 @@ export function CreateGoal() {
                 id="title"
                 autoFocus
                 placeholder="Praticar exercícios, meditar, etc"
+                {...register('title')}
               />
+
+              {formState.errors.title && (
+                <p className="text-red-400 text-sm">
+                  {formState.errors.title.message}
+                </p>
+              )}
             </div>
 
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Quantas vezes na semana?</Label>
-              <RadioGroup>
-                <RadioGroupItem value="1">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    1x vez na semana
-                  </span>
-                  <span className="text-lg leading-none">🥱</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="2">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    2x vez na semana
-                  </span>
-                  <span className="text-lg leading-none">🙂</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="3">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    3x vez na semana
-                  </span>
-                  <span className="text-lg leading-none">😎</span>
-                </RadioGroupItem>
-              </RadioGroup>
+
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={3}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      {weeklyOptions.map(weeklyOption => (
+                        <RadioGroupItem
+                          key={weeklyOption.value}
+                          value={weeklyOption.value}
+                        >
+                          <RadioGroupIndicator />
+                          <span className="text-zinc-300 text-sm font-medium leading-none">
+                            {weeklyOption.title}
+                          </span>
+                          <span className="text-lg leading-none">
+                            {weeklyOption.icon}
+                          </span>
+                        </RadioGroupItem>
+                      ))}
+                    </RadioGroup>
+                  )
+                }}
+              />
             </div>
           </div>
 
